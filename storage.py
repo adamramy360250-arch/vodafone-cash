3eb3afd05c055450018f9a475bbd3922ccccfb2c
adamramy360250-arch <adamramy360250@gmail.com>
2026-05-14 02:08:53 +0300

    fix storage save

--- storage.py
+++ storage.py
@@ -1,9 +1,17 @@
 import json,os
 F=os.path.expanduser("~/.vcash.json")
-def save(d):
+def save(seamless,token,msisdn):
+    d={}
+    if os.path.exists(F):
+        try:d=json.load(open(F))
+        except:d={}
+    d["seamless"]=seamless
+    d["token"]=token
+    d["msisdn"]=msisdn
     open(F,"w").write(json.dumps(d))
 def load():
     if not os.path.exists(F):return {}
-    return json.load(open(F))
+    try:return json.load(open(F))
+    except:return {}
 def clear():
     if os.path.exists(F):os.remove(F)
